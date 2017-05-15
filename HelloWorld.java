b304879fb955d642bcee8e4420b4a32b2ac92626
root <root@jenkins1>
2017-05-15 06:36:22 +0000

    root

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Test")
-		System.out.println("How are you Balaji and Shiva")
+		System.out.println("Test");
+		System.out.println("How are you Balaji and Shiva");
 	}
 }
