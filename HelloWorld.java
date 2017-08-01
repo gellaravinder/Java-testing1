a6fb51c492b3a30a3a109be93df0f778faba7a78
gellaravinder <gella.ravinder@gmail.com>
2017-08-01 07:07:20 +0530

    edit

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Test");
-		System.out.println("How are you Vinod, Rajeshand Aniaha");
+		System.out.println("How are you Vinod, Rajesh and Anusha");
 	}
 }
