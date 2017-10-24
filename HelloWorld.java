f32c09969fea86841d6f371668cfef20f41a71fe
root <root@Node3>
2017-10-24 01:38:38 +0000

    new line added

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld
 		System.out.println("Hello World!");
 		System.out.println("Test - THis is a code testing");
 		System.out.println("added new line from git - DevOps5 ");
+		System.out.println("added second line for testin 24/10/2017 - DevOps5 ");
 		System.out.println("=================================== ");
 
 	System.out.println("added new line from git Local - DevOps5 ");
